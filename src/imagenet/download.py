158c0b8343a83ad5aab177184fb3e3ec2bf76f4f
theyoucheng <youcheng.sun@cs.ox.ac.uk>
2018-07-15 23:25:28 +0100

    remove redundancy

--- src/imagenet/download.py
+++
@@ -1,9 +0,0 @@
-
-import os
-with open("synset.txt") as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
-content = [x.strip() for x in content] 
-
-for c in content:
-  os.system('wget {0}'.format(c))
