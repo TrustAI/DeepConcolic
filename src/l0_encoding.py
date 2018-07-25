4909b9fd0a8037f63fa73425ebeffb7bdcab1e7c
theyoucheng <youcheng.sun@cs.ox.ac.uk>
2018-07-25 14:52:43 +0000

    fix python 2/3 inconsistency

--- src/l0_encoding.py
+++ src/l0_encoding.py
@@ -93,7 +93,8 @@ def refine_act_image(dnn, layer_functions, image, nc_layer, pos, sorted_pixels, 
   idx_range=np.arange(idx_first)
   while True:
     length=len(idx_range)
-    for i in range(0, idx_first):
+    print ('idx_first: ', idx_first)
+    for i in range(0, idx_first[0]):
       pixel_row = sorted_pixels[i, 0].astype('int')
       pixel_col = sorted_pixels[i, 1].astype('int')
       refined_act_image[pixel_row, pixel_col] = image[pixel_row, pixel_col]
