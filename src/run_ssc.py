3f1a897009ebd7e10d7c46417ee94ae4b9f0c7ab
theyoucheng <youcheng.sun@cs.ox.ac.uk>
2018-07-30 14:42:35 +0100

    added ssc map

---
+++ src/run_ssc.py
@@ -0,0 +1,42 @@
+import argparse
+import sys
+import os
+from datetime import datetime
+
+import keras
+from keras.models import *
+from keras.datasets import cifar10
+from keras.datasets import mnist
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.layers import *
+from keras import *
+from utils import *
+from nc_setup import *
+
+def run_ssc(test_object, outs):
+  print ('To run ssc\n')
+  
+  f_results, layer_functions, cover_layers, activations, test_cases, adversarials=nc_setup(test_object, outs)
+
+  while True:
+    dec_layer_index, dec_pos=get_ssc_next(cover_layers)
+
+    print ('== to cover (dec_layer_index, dec_pos)', dec_layer_index, dec_pos)
+
+    ###
+    #cond_layer=cover_layers[cond_layer_index]
+    #dec_layer=cover_layers[dec_layer_index]
+    #cond_pos_=np.unravel(cond_pos, (cond_layer.activations[0]).shape)
+    #dec_pos_=np.unravel(dec_pos, (dec_layer.activations[0]).shape)
+    ###
+
+    #feasible, d, new_im, old_image=ssc_search(test_object, layer_functions, cond_layer, cond_pos_, dec_layer, dec_pos_)
+
+    #if feasible:
+    #  test_cases.append((new_image, old_image))
+    #else:
+    #  print ("not feasible")
+
+    #print ('let us finish the SSC run here...')
+    #break
