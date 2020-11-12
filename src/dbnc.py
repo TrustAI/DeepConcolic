TrustAI/DeepConcolic

commit 45bd257a947faa748b14a82708d112ae3c6224b6

Introduce the ability to fix extracted latent feature components event in LP problem if they are not represented by any node of the BN

src/dbnc.py
@@ -323,11 +323,23 @@ class BFcLayer (CoverableLayer):
     assert isinstance (discretization, FeatureDiscretizer)
     self.transform = transform
     self.discr = discretization
-    self.skip = 0
+    self.first = 0
+    self.last = -1
 
   def get_params (self, deep = True):
     return dict (name = self.layer.name,
                  transform = self.transform.get_params (deep))
+
+
+  def feature_of_component (self, component: int) -> Optional[int]:
+    if self.first <= component and (self.last < 0 or component < self.last):
+      return component - self.first
+    else:
+      return None
+
+
+  def range_components (self) -> range:
+    return range (len (self.transform[-1].components_))
 
 
   @property
@@ -335,10 +347,10 @@ class BFcLayer (CoverableLayer):
     '''
     Number of extracted features for the layer.
     '''
-    return len (self.transform[-1].components_) - self.skip
+    return len (self.transform[-1].components_[self.first:self.last])
 
 
-  def range_features (self):
+  def range_features (self) -> range:
     '''
     Range over all feature indexes.
     '''
@@ -358,9 +370,10 @@ class BFcLayer (CoverableLayer):
     return acc
 
 
-  def dimred_activations (self, acts, acc = None):
+  def dimred_activations (self, acts, acc = None, feature_space = True):
     x = np.vstack([a.flatten () for a in acts[self.layer_index]])
-    y = self.transform.transform (x)[:,self.skip:]
+    y = self.transform.transform (x)
+    y = y[:,self.first:self.last] if feature_space else y
     acc = np.hstack ((acc, y)) if acc is not None else y
     del x
     if acc is not y: del y
@@ -369,7 +382,7 @@ class BFcLayer (CoverableLayer):
 
   def dimred_n_discretize_activations (self, acts, acc = None):
     x = np.vstack([a.flatten () for a in acts[self.layer_index]])
-    y = self.discr.transform (self.transform.transform (x)[:,self.skip:])
+    y = self.discr.transform (self.transform.transform (x)[:,self.first:self.last])
     acc = np.hstack ((acc, y.astype (int))) if acc is not None else y.astype (int)
     del x, y
     return acc
@@ -501,10 +514,10 @@ class _BaseBFcCriterion (Criterion):
     return acc
 
 
-  def dimred_activations (self, acts, fls = None):
+  def dimred_activations (self, acts, fls = None, **kwds):
     acc = None
     for fl in self.flayers if fls is None else fls:
-      acc = fl.dimred_activations (acts, acc = acc)
+      acc = fl.dimred_activations (acts, acc = acc, **kwds)
     return acc
 
 
@@ -786,20 +799,21 @@ class _BaseBFcCriterion (Criterion):
         fl.transform.fit (copy.copy (x_ok[:fts]))
         y_ok = fl.transform.transform (x_ok)
       p1 ('| Extracted {} feature{}'.format (*s_(y_ok.shape[1])))
-      if fl.skip > 0:
-        p1 ('| Skipping {} feature{}'.format (*s_(fl.skip)))
+      if fl.first > 0:
+        p1 ('| Skipping {} important feature{}'.format (*s_(fl.first)))
 
       fit_wrt_args = {}
+      x_ko, y_ko = [], []
       if ko_acts != {}:
         x_ko = np.stack([a.flatten () for a in ko_acts[fl.layer_index]], axis = 0)
         y_ko = fl.transform.transform (x_ko)
-        fit_wrt_args = dict (y2plot = y_ko[:,fl.skip:],
+        fit_wrt_args = dict (y2plot = y_ko[:,fl.first:fl.last],
                              y2plot_labels = ko_labels)
 
       tp1 ('Discretizing features...')
 
-      fl.discr.fit_wrt (x_ok[:,fl.skip:],
-                        y_ok[:,fl.skip:],
+      fl.discr.fit_wrt (x_ok[:,fl.first:fl.last],
+                        y_ok[:,fl.first:fl.last],
                         fl.transform,
                         layer = fl,
                         **kwds,
@@ -814,7 +828,7 @@ class _BaseBFcCriterion (Criterion):
       del x_ok, y_ok, x_ko, y_ko
 
     self.explained_variance_ratios_ = \
-      { str(fl): (fl.transform[-1].explained_variance_ratio_[fl.skip:].tolist (),
+      { str(fl): (fl.transform[-1].explained_variance_ratio_[fl.first:fl.last].tolist (),
                   fl.transform[-1].explained_variance_ratio_.tolist ())
         for fl in self.flayers
         if hasattr (fl.transform[-1], 'explained_variance_ratio_') }

src/dbnc_pulp.py
@@ -20,7 +20,7 @@ class PulpBFcAbstrLayerEncoder (PulpStrictLayerEncoder):
     self.flayer = fl
 
 
-  def pulp_output_feature_linear_expression (self, feature: int):
+  def pulp_output_component_linear_expression (self, component: int):
 
     transform = self.flayer.transform
 
@@ -37,19 +37,19 @@ class PulpBFcAbstrLayerEncoder (PulpStrictLayerEncoder):
                         .format (transform[-1]))
 
     o = self.pulp_out_exprs ()
-    m = transform[-1].mean_[self.flayer.skip:]
-    c = transform[-1].components_[self.flayer.skip:]
+    m = transform[-1].mean_
+    c = transform[-1].components_
 
     if isinstance (transform[0], StandardScaler):
-      u = transform[0].mean_[self.flayer.skip:]
-      s = transform[0].scale_[self.flayer.skip:]
+      u = transform[0].mean_
+      s = transform[0].scale_
       lin_expr = lpSum ([LpAffineExpression ([(o, c / s)], - (u / s) * c - m * c)
                          for (o, u, s, m, c) in
-                         zip (o.flatten (), u, s, m, c[feature].T)])
+                         zip (o.flatten (), u, s, m, c[component].T)])
     else:
       lin_expr = lpSum ([LpAffineExpression ([(o, c)], - m * c)
                          for (o, m, c) in
-                         zip (o.flatten (), m, c[feature].T)])
+                         zip (o.flatten (), m, c[component].T)])
 
     # if True:                 # Filter-out terms where coeff < epsilon:
     #   lin_expr = LpAffineExpression ([ (x, a) for (x, a) in lin_expr.items ()
@@ -57,6 +57,10 @@ class PulpBFcAbstrLayerEncoder (PulpStrictLayerEncoder):
     #                                  lin_expr.constant)
 
     return lin_expr
+
+
+  def pulp_output_feature_linear_expression (self, feature: int):
+    return self.pulp_output_component_linear_expression (self.flayer.first + feature)
 
 
 
@@ -81,29 +85,24 @@ class PulpBFcAbstrLayerEncoder (PulpStrictLayerEncoder):
     return cstrs
 
 
-  def pulp_replicate_feature_value (self, feature: int, value: float, approx = False):
-    lin_expr = self.pulp_output_feature_linear_expression (feature)
+
+  def pulp_replicate_component_value (self, component: int, value: float, approx = False):
+    lin_expr = self.pulp_output_component_linear_expression (component)
     if approx:
       return [ LpConstraint (lin_expr, LpConstraintGE,
-                             '{}_low_{}'.format(self.flayer, feature),
+                             '{}_low_{}'.format(self.flayer, component),
                              value - epsilon),
                LpConstraint (lin_expr, LpConstraintLE,
-                             '{}_up_{}'.format(self.flayer, feature),
+                             '{}_up_{}'.format(self.flayer, component),
                              value + epsilon)]
     else:
       return [ LpConstraint (lin_expr, LpConstraintEQ,
-                             '{}_eq_{}'.format(self.flayer, feature),
+                             '{}_eq_{}'.format(self.flayer, component),
                              value) ]
 
 
-  # def pulp_replicate_activations_outside_of_feature (self, feature: int, ap_x,
-  #                                                    prev: PulpLayerOutput):
-  #   o = self.pulp_out_exprs ()
-  #   c = self.flayer.transform[-1].components_[feature].reshape(o.shape)
-  #   m = self.flayer.transform[-1].mean_.reshape(o.shape)
-  #   return self.pulp_replicate_activations (
-  #     ap_x, prev, exclude = lambda i: abs(c[i] # * m[i]
-  #                                         ) > epsilon)
+  def pulp_replicate_feature_value (self, feature: int, value: float, **kwds):
+    return pulp_replicate_component_value (self.flayer.first + feature, value, **kwds)
 
 
 # ---
@@ -149,6 +148,22 @@ class _BFcPulpAnalyzer (Analyzer, PulpSolver4DNN):
       return self.metric.distance (x, res[1]), res[1]
 
 
+  def constrain_target_interval(self, lc, feature, feature_part, activations):
+    cstrs = lc.pulp_constrain_outputs_in_feature_part (feature, feature_part)
+
+    if self.fix_untargetted_features:
+      dimred = lc.flayer.dimred_activations (activations,
+                                             feature_space = False)[0]
+      for component in lc.flayer.range_components ():
+        cfeat = lc.flayer.feature_of_component (component)
+        if cfeat is not None and cfeat == feature:
+          continue
+        cstrs.extend (lc.pulp_replicate_component_value \
+                         (component, dimred[component], approx = False))
+
+    return cstrs
+
+
 # ---
 
 from dbnc import BFcTarget, BFcAnalyzer
@@ -167,20 +182,8 @@ class BFcPulpAnalyzer (_BFcPulpAnalyzer, BFcAnalyzer):
       cstrs.extend (le.pulp_replicate_behavior (activations, prev))
       prev = le
 
-    dimred_activations = lc.flayer.dimred_activations (activations)[0] \
-                         if self.fix_untargetted_features else None
-    for feature in lc.flayer.range_features ():
-      if feature == target.fnode.feature:
-        cstrs.extend (lc.pulp_constrain_outputs_in_feature_part \
-                      (target.fnode.feature, target.feature_part))
-      elif self.fix_untargetted_features:
-        cstrs.extend (lc.pulp_replicate_feature_value \
-                      (feature, dimred_activations[feature],
-                       approx = False))
-
-    # cstrs.extend(lc.pulp_replicate_activations_outside_of_feature (
-    #   target.fnode.feature, self.eval (x),
-    #   self.layer_encoders[target.fnode.flayer.layer_index - 1]))
+    cstrs.extend (self.constrain_target_interval ( \
+      lc, target.fnode.feature, target.feature_part, activations))
 
     return self.actual_search (problem, x, cstrs, target)
 
@@ -209,20 +212,8 @@ class BFDcPulpAnalyzer (BFcPulpAnalyzer, BFDcAnalyzer):
     for f0, f0p in enumerate (target.feature_parts0):
       cstrs.extend (lc0.pulp_constrain_outputs_in_feature_part (f0, f0p))
 
-    dimred_activations = lc1.flayer.dimred_activations (activations)[0] \
-                         if self.fix_untargetted_features else None
-    for feature in lc1.flayer.range_features ():
-      if feature == target.fnode1.feature:
-        cstrs.extend (lc1.pulp_constrain_outputs_in_feature_part \
-                      (target.fnode1.feature, target.feature_part1))
-      elif self.fix_untargetted_features:
-        cstrs.extend (lc1.pulp_replicate_feature_value \
-                      (feature, dimred_activations[feature],
-                       approx = False))
-
-    # cstrs.extend(lc.pulp_replicate_activations_outside_of_feature (
-    #   target.fnode.feature, self.eval (x),
-    #   self.layer_encoders[target.fnode.flayer.layer_index - 1]))
+    cstrs.extend (self.constrain_target_interval ( \
+      lc1, target.fnode1.feature, target.feature_part1, activations))
 
     return self.actual_search (problem, x, cstrs, target)
 

src/dbnc_stats.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 # ---
 
-class FakeBFcAnalyzer (BFcAnalyzer, BFDcAnalyzer):
+class FakeBFcAnalyzer (BFDcAnalyzer):
 
   def finalize_setup(self, _clayers):
     pass

commit a628dadf6929bf201b3d703c7d6cfd12c3964b41

Fix bug introduced in 45bd257a947faa748b14a82708d112ae3c6224b6

src/dbnc.py
@@ -324,7 +324,12 @@ class BFcLayer (CoverableLayer):
     self.transform = transform
     self.discr = discretization
     self.first = 0
-    self.last = -1
+    self.last = None
+
+  @property
+  def focus (self) -> slice:
+    return slice (self.first, self.last)
+
 
   def get_params (self, deep = True):
     return dict (name = self.layer.name,
@@ -332,7 +337,7 @@ class BFcLayer (CoverableLayer):
 
 
   def feature_of_component (self, component: int) -> Optional[int]:
-    if self.first <= component and (self.last < 0 or component < self.last):
+    if self.first <= component and (self.last is None or component <= self.last):
       return component - self.first
     else:
       return None
@@ -347,7 +352,7 @@ class BFcLayer (CoverableLayer):
     '''
     Number of extracted features for the layer.
     '''
-    return len (self.transform[-1].components_[self.first:self.last])
+    return len (self.transform[-1].components_[self.focus])
 
 
   def range_features (self) -> range:
@@ -373,7 +378,7 @@ class BFcLayer (CoverableLayer):
   def dimred_activations (self, acts, acc = None, feature_space = True):
     x = np.vstack([a.flatten () for a in acts[self.layer_index]])
     y = self.transform.transform (x)
-    y = y[:,self.first:self.last] if feature_space else y
+    y = y[:,self.focus] if feature_space else y
     acc = np.hstack ((acc, y)) if acc is not None else y
     del x
     if acc is not y: del y
@@ -382,7 +387,7 @@ class BFcLayer (CoverableLayer):
 
   def dimred_n_discretize_activations (self, acts, acc = None):
     x = np.vstack([a.flatten () for a in acts[self.layer_index]])
-    y = self.discr.transform (self.transform.transform (x)[:,self.first:self.last])
+    y = self.discr.transform (self.transform.transform (x)[:,self.focus])
     acc = np.hstack ((acc, y.astype (int))) if acc is not None else y.astype (int)
     del x, y
     return acc
@@ -807,13 +812,13 @@ class _BaseBFcCriterion (Criterion):
       if ko_acts != {}:
         x_ko = np.stack([a.flatten () for a in ko_acts[fl.layer_index]], axis = 0)
         y_ko = fl.transform.transform (x_ko)
-        fit_wrt_args = dict (y2plot = y_ko[:,fl.first:fl.last],
+        fit_wrt_args = dict (y2plot = y_ko[:,fl.focus],
                              y2plot_labels = ko_labels)
 
       tp1 ('Discretizing features...')
 
-      fl.discr.fit_wrt (x_ok[:,fl.first:fl.last],
-                        y_ok[:,fl.first:fl.last],
+      fl.discr.fit_wrt (x_ok[:,fl.focus],
+                        y_ok[:,fl.focus],
                         fl.transform,
                         layer = fl,
                         **kwds,
@@ -828,7 +833,7 @@ class _BaseBFcCriterion (Criterion):
       del x_ok, y_ok, x_ko, y_ko
 
     self.explained_variance_ratios_ = \
-      { str(fl): (fl.transform[-1].explained_variance_ratio_[fl.first:fl.last].tolist (),
+      { str(fl): (fl.transform[-1].explained_variance_ratio_[fl.focus].tolist (),
                   fl.transform[-1].explained_variance_ratio_.tolist ())
         for fl in self.flayers
         if hasattr (fl.transform[-1], 'explained_variance_ratio_') }