b5cc334bc99e879a44feb194a346568e66f7f7c3
Nicolas Berthier <m@nberth.space>
2020-12-22 17:06:49 +0100

    Large model for Fashion-MNIST

--- src/gen_fashion_mnist.py
+++ src/gen_fashion_mnist.py
@@ -44,6 +44,60 @@ def make_medium_maxp_model (input_shape, **kwds):
         tf.keras.layers.Activation('softmax'),
     ], **kwds)
 
+# def make_large_maxp_model (input_shape, **kwds):
+#     return tf.keras.models.Sequential([
+#         tf.keras.layers.Conv2D(32, (3, 3), input_shape = input_shape),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Conv2D(32, (3, 3)),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.MaxPooling2D((2, 2)),
+#         tf.keras.layers.Conv2D(64, (2, 2)),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Conv2D(64, (2, 2)),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Conv2D(64, (2, 2)),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.MaxPooling2D((2, 2)),
+#         tf.keras.layers.Flatten(),
+#         tf.keras.layers.Dense(100),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Dense(100),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Dense(100),
+#         tf.keras.layers.Activation('relu'),
+#         tf.keras.layers.Dense(10),
+#         tf.keras.layers.Activation('softmax'),
+#     ], **kwds)
+
+def make_large_model (input_shape, **kwds):
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), input_shape = input_shape),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Conv2D(16, (3, 3)),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(24, (3, 3)),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Conv2D(24, (3, 3)),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Conv2D(24, (3, 3)),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(128),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(92),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(92),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Dense(10),
+        tf.keras.layers.Activation('softmax'),
+    ], **kwds)
+
 # classifier (load_data, make_small_model,
 #             model_name = 'fashion_mnist_small',
 #             outdir = '/tmp',
@@ -54,8 +108,20 @@ def make_medium_maxp_model (input_shape, **kwds):
 #             outdir = '/tmp',
 #             cm_plot_args = dict (hrotate = True))
 
-classifier (load_data, make_medium_maxp_model,
-            model_name = 'fashion_mnist_medium_maxp',
+# classifier (load_data, make_medium_maxp_model,
+#             model_name = 'fashion_mnist_medium_maxp',
+#             outdir = '/tmp',
+#             epochs = 50,
+#             cm_plot_args = dict (hrotate = True))
+
+# classifier (load_data, make_large_maxp_model,
+#             model_name = 'fashion_mnist_large_maxp',
+#             outdir = '/tmp',
+#             epochs = 50,
+#             cm_plot_args = dict (hrotate = True))
+
+classifier (load_data, make_large_model,
+            model_name = 'fashion_mnist_large',
             outdir = '/tmp',
             epochs = 50,
             cm_plot_args = dict (hrotate = True))
